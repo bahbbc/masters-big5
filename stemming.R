b01fcc685a4d10260621d36753158ae1608d8db7
Barbara Barbosa <barbarab.claudino@gmail.com>
2016-11-18 09:48:05 -0200

    Add normalized file and stemming script

--- stemming.R
+++ stemming.R
@@ -1,27 +1,49 @@
 library(tm)
 library(ggplot2)
 
+personalities <- read.csv('final_normalized_file.csv')
 
-corpus <- read.csv('~/workspace/masters/post/17-vocabulary.csv', header = TRUE, encoding="latin1", stringsAsFactors=FALSE, sep = ';')
+generate_word_martrix <- function(files){
+  all_words <- data.frame()
+  for (file_name in files){
+    corpus <- read.csv(file_name, header = TRUE, encoding="latin1", stringsAsFactors=FALSE, sep = ';')
+    corpus <- rep(corpus$word, corpus$frequency)
+    
+    collapsed_text = paste(unlist(corpus), collapse=' ')
+    
+    file_name <- gsub('/home/barbara/workspace/masters/post/', '', file_name)
+    file_name <- gsub('-vocabulary.csv', '', file_name)
+    
+    concatened_files <- cbind(collapsed_text, file_name)
+    
+    all_words <- rbind(all_words, concatened_files)
+  }
+  all_words
+}
 
-corpus <- rep(corpus$word, corpus$frequency)
+generate_frequency_matrix <- function(word_matrix){
+  c <- Corpus(VectorSource(word_matrix))
+  
+  c <- tm_map(c, stemDocument, language = "portuguese")
+  
+  docs <- tm_map(c, PlainTextDocument)
+  
+  dtm <- DocumentTermMatrix(docs)
+  
+  freq <- colSums(as.matrix(dtm))
+  
+  ord <- order(freq)
+  
+  term_freq <- subset((freq), freq >= 1)
+  
+  df <- data.frame(term = names(term_freq), freq = term_freq)
+  df <- df[order(term_freq),]
+}
 
-c <- Corpus(VectorSource(corpus))
+folders <- list.dirs('~/workspace/masters/post')
 
-c <- tm_map(c, stemDocument, language = "portuguese")
+file_names <- list.files(folders, full.names=TRUE, pattern='[0-9]')
 
-docs <- tm_map(c, PlainTextDocument)
-
-dtm <- DocumentTermMatrix(docs)
-
-freq <- colSums(as.matrix(dtm))
-length(freq)
-
-ord <- order(freq)
-
-term_freq <- subset((freq), freq >= 2)
-
-df <- data.frame(term = names(term_freq), freq = term_freq)
-df <- df[order(term_freq),]
+final_word_matrix <- generate_word_martrix(file_names)
 
 ggplot(df, aes(x = df$term, y = df$freq)) + geom_bar(stat = "identity") + xlab("Terms") + ylab("Count") + coord_flip()
