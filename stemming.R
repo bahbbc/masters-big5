9d5434e8781022bc2ddb763613d7fb6a887586a6
Barbara Barbosa <barbarab.claudino@gmail.com>
2016-11-20 22:07:54 -0200

    Fix everything to use the tm library

--- stemming.R
+++ stemming.R
@@ -1,49 +1,31 @@
 library(tm)
 library(ggplot2)
 
-personalities <- read.csv('final_normalized_file.csv')
+personalities <- read.csv('~/workspace/masters/final_normalized_file.csv')
 
-generate_word_martrix <- function(files){
-  all_words <- data.frame()
-  for (file_name in files){
-    corpus <- read.csv(file_name, header = TRUE, encoding="latin1", stringsAsFactors=FALSE, sep = ';')
-    corpus <- rep(corpus$word, corpus$frequency)
-    
-    collapsed_text = paste(unlist(corpus), collapse=' ')
-    
-    file_name <- gsub('/home/barbara/workspace/masters/post/', '', file_name)
-    file_name <- gsub('-vocabulary.csv', '', file_name)
-    
-    concatened_files <- cbind(collapsed_text, file_name)
-    
-    all_words <- rbind(all_words, concatened_files)
-  }
-  all_words
-}
-
-generate_frequency_matrix <- function(word_matrix){
-  c <- Corpus(VectorSource(word_matrix))
+# verificar como esse codigo funciona para importar todos os arquivos de uma pasta
+c <- Corpus(DirSource('~/workspace/masters/post', encoding="latin1"), readerControl = list(language="portuguese"))
   
-  c <- tm_map(c, stemDocument, language = "portuguese")
+c <- tm_map(c, stemDocument, language = "portuguese")
+c <- tm_map(c, removePunctuation) 
+c <- tm_map(c, removeNumbers)
   
-  docs <- tm_map(c, PlainTextDocument)
+dtm <- DocumentTermMatrix(c)
   
-  dtm <- DocumentTermMatrix(docs)
+tdm <- DocumentTermMatrix(c)
   
-  freq <- colSums(as.matrix(dtm))
+tm_matrix <- as.matrix(tdm)
   
-  ord <- order(freq)
+tm_data <- as.data.frame(tm_matrix)
   
-  term_freq <- subset((freq), freq >= 1)
+file_name <- gsub('/home/barbara/workspace/masters/post/', '', rownames(tm_data))
+file_name <- gsub('-vocabulary.csv', '', file_name)
+tm_data <- cbind(tm_data, file_name)
   
-  df <- data.frame(term = names(term_freq), freq = term_freq)
-  df <- df[order(term_freq),]
-}
-
-folders <- list.dirs('~/workspace/masters/post')
-
-file_names <- list.files(folders, full.names=TRUE, pattern='[0-9]')
-
-final_word_matrix <- generate_word_martrix(file_names)
+personalities_tmp <- data.frame(id = personalities$id, extraversion = personalities$extraversion, agreeableness = personalities$agreeableness, conscientiousness = personalities$conscientiousness, neuroticism = personalities$neuroticism, openness = personalities$openness)
+  
+personality_terms <- merge(personalities_tmp, tm_data, by.x= 'id', by.y = 'file_name')
+  
+write.csv(tm_data, 'personality_terms.csv')
 
 ggplot(df, aes(x = df$term, y = df$freq)) + geom_bar(stat = "identity") + xlab("Terms") + ylab("Count") + coord_flip()
