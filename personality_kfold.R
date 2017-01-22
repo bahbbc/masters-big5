04e1c6ff1620799d571ccbe755142f13194e0fc8
Barbara Barbosa <barbarab.claudino@gmail.com>
2017-01-22 11:00:00 -0200

    Fix wrong class for openess

--- personality_kfold.R
+++ personality_kfold.R
@@ -19,7 +19,7 @@ fold_measures <- function(dataset, attributes, class){
     precision <- tb[4]/(tb[4] + tb[3])
     recall <- tb[4]/(tb[4] + tb[2])
     fmeasure <- 2 * ((precision * recall)/(precision + recall))
-    accuracy <- (tb[1] + tb[4])/(tb[1] + tb[2] + tb[1] + tb[3] + tb[1] + tb[4])
+    accuracy <- (tb[1] + tb[4])/(tb[1] + tb[2] + tb[3] + tb[4])
     metrics <- c('fmeasure' = fmeasure, 'precision' = precision, 'recall' = recall, 'accuracy' = accuracy)
     measure_mean <- rbind(measure_mean, metrics)
   }
@@ -43,9 +43,13 @@ personalities <- read.csv('personality-normalized.csv')
 personalities <- sapply(personalities, function(x) as.numeric(as.matrix(x)))
 personalities <- as.data.frame(personalities)
 
+
+# remove people with less than 1st quartile words
+new_personalities <- personalities[personalities$items > 780,]
+
 # remove NAs
 
-new_personalities <- personalities[!is.na(personalities$exclam),]
+# new_personalities <- personalities[!is.na(personalities$exclam),]
 
 ### EXTRAVERSION
 
@@ -73,8 +77,6 @@ fold_measures(new_personalities, attributes, 'extraversion_m')
 
 ### AGREEABLENESS
 
-data_set <- create_partition('agreeabeness_m')
-
 ### PRE PROC
 print('PRE PROC')
 attributes <- (as.factor(agreeabeness_m) ~ skip + compound+ hashtags+ links+ punct + questions+ exclam+ numbers+ upcase+ lowcase+ firstup+ pt.lexicon + added+ verb.pro+ names+ en.lexicon+ rewrite+ mispell+ foreign+ emo. + emo..1 + emo..2 + laugh+ emph+ echars+ unkn)
@@ -99,8 +101,6 @@ fold_measures(new_personalities, attributes, 'agreeabeness_m')
 
 #CONSCIENTIOUSNESS
 
-data_set <- create_partition('conscientiousness_m')
-
 #SOCIAL
 attributes <- (as.factor(conscientiousness_m) ~ skip + compound+ hashtags+ links+ punct + questions+ exclam+ numbers+ upcase+ lowcase+ firstup+ pt.lexicon + added+ verb.pro+ names+ en.lexicon+ rewrite+ mispell+ foreign+ emo. + emo..1 + emo..2 + laugh+ emph+ echars+ unkn)
 fold_measures(new_personalities, attributes, 'conscientiousness_m')
@@ -121,8 +121,6 @@ attributes <- (as.factor(conscientiousness_m) ~ sentences + items + chars + allT
 fold_measures(new_personalities, attributes, 'conscientiousness_m')
 
 ############################################
-
-data_set <- create_partition('neuroticism_m')
 
 # NEUROTICISM
 
@@ -148,8 +146,6 @@ fold_measures(new_personalities, attributes, 'neuroticism_m')
 ################
 
 # OPENESS
-
-data_set <- create_partition('openness_m')
 
 #SOCIAL
 attributes <- (as.factor(openness_m) ~ skip + compound+ hashtags+ links+ punct + questions+ exclam+ numbers+ upcase+ lowcase+ firstup+ pt.lexicon + added+ verb.pro+ names+ en.lexicon+ rewrite+ mispell+ foreign+ emo. + emo..1 + emo..2 + laugh+ emph+ echars+ unkn)
@@ -285,21 +281,21 @@ data_set <- create_ober_partition('openness_ober_2')
 ### PRE PROC
 print('PRE PROC')
 attributes <- (as.factor(openness_ober_2) ~ skip + compound+ hashtags+ links+ punct + questions+ exclam+ numbers+ upcase+ lowcase+ firstup+ pt.lexicon + added+ verb.pro+ names+ en.lexicon+ rewrite+ mispell+ foreign+ emo. + emo..1 + emo..2 + laugh+ emph+ echars+ unkn)
-fold_measures(data_set, attributes, 'neuroticism_ober_2')
+fold_measures(data_set, attributes, 'openness_ober_2')
 
 ### GRAMMAR
 print('GRAMMAR')
 attributes <- (as.factor(openness_ober_2) ~ m+f+s+p+aument+dimin+superlat+N+A+PREP+CONJ+ADV+PREFIX+SIGLA+ABREV+INTERJ+DET+def+indef+NUM+numC+numO+numM+numF+PRO+proDem+proIndef+proRel+proInterr+proTrat+proPoss+proPess+acusativa+dativa+nominativa+obliqua+reflexa+p1+p2+p3+V+VW+VG+VK+VP+VI+VJ+VF+VQ+VS+VT+VU+VY+VC+V1s+V2s+V3s+V1p+V2p+V3p)
-fold_measures(data_set, attributes, 'neuroticism_ober_2')
+fold_measures(data_set, attributes, 'openness_ober_2')
 
 ### LIWC
 print('LIWC')
 attributes <- as.factor(openness_ober_2) ~ X1funct + X2pronoun + X3ppron +  X4i +  X5we +  X6you +  X7shehe +  X8they +  X9ipron +  X10article +  X11verb +  X12auxverb +  X13past +  X14present +  X15future +  X16adverb +  X17preps +  X18conj +  X19negate +  X20quant +  X21number +  X22swear +  X23social +  X24family +  X25friend +  X26humans +  X27affect +  X28posemo +  X29negemo +  X30anx +  X31anger +  X32sad +  X33cogmech +  X34insight +  X35cause +  X36discrep +  X37tentat +  X38certain +  X39inhib +  X40incl +  X41excl +  X42percept +  X43see +  X44hear +  X45feel +  X46bio +  X47body +  X48health +  X49sexual +  X50ingest +  X51relativ +  X52motion +  X53space +  X54time +  X55work +  X56achieve +  X57leisure +  X58home +  X59money +  X60relig +  X61death +  X62assent +  X63nonfl +  X64filler 
-fold_measures(data_set, attributes, 'neuroticism_ober_2')
+fold_measures(data_set, attributes, 'openness_ober_2')
 
 ### ALL
 print('ALL')
 attributes <- (as.factor(openness_ober_2) ~ sentences + items + chars + allTokens + wordTokens +  m+f+s+p+aument+dimin+superlat+N+A+PREP+CONJ+ADV+PREFIX+SIGLA+ABREV+INTERJ+DET+def+indef+NUM+numC+numO+numM+numF+PRO+proDem+proIndef+proRel+proInterr+proTrat+proPoss+proPess+acusativa+dativa+nominativa+obliqua+reflexa+p1+p2+p3+V+VW+VG+VK+VP+VI+VJ+VF+VQ+VS+VT+VU+VY+VC+V1s+V2s+V3s+V1p+V2p+V3p+X1funct + X2pronoun + X3ppron +  X4i +  X5we +  X6you +  X7shehe +  X8they +  X9ipron +  X10article +  X11verb +  X12auxverb +  X13past +  X14present +  X15future +  X16adverb +  X17preps +  X18conj +  X19negate +  X20quant +  X21number +  X22swear +  X23social +  X24family +  X25friend +  X26humans +  X27affect +  X28posemo +  X29negemo +  X30anx +  X31anger +  X32sad +  X33cogmech +  X34insight +  X35cause +  X36discrep +  X37tentat +  X38certain +  X39inhib +  X40incl +  X41excl +  X42percept +  X43see +  X44hear +  X45feel +  X46bio +  X47body +  X48health +  X49sexual +  X50ingest +  X51relativ +  X52motion +  X53space +  X54time +  X55work +  X56achieve +  X57leisure +  X58home +  X59money +  X60relig +  X61death +  X62assent +  X63nonfl +  X64filler + skip + compound+ hashtags+ links+ punct + questions+ exclam+ numbers+ upcase+ lowcase+ firstup+ pt.lexicon + added+ verb.pro+ names+ en.lexicon+ rewrite+ mispell+ foreign+ emo. + emo..1 + emo..2 + laugh+ emph+ echars+ unkn)
-fold_measures(data_set, attributes, 'neuroticism_ober_2')
+fold_measures(data_set, attributes, 'openness_ober_2')
 
 ############################
