4a5abcc202b68baabdad0059f56de1b43d38ffa3
Barbara Barbosa <barbarab.claudino@gmail.com>
2017-02-16 07:59:12 -0200

    Add all personalities files

--- normalize_posts_to_file.R
+++ normalize_posts_to_file.R
@@ -3,14 +3,14 @@
 tokenizing <- function(original_files, concatenated_files){
   # if the merged dataset does exist, append to it
   for (file in original_files){
-    text <-readLines(file, encoding="UTF-8")
+    text <-readLines(file, encoding="latin1")
     # Save file_name to match personality
     file_name <- as.integer(text[1])
     
     # generate one whole string with the text
     formatted_text = paste(unlist(text), collapse=' ')
     # remove numbers
-    formatted_text = gsub("[0-9]", " ", tolower(formatted_text))
+    formatted_text = gsub("[0-9]", " ", formatted_text)
     # Remove '"'
     formatted_text = gsub("\"", " ", formatted_text)
     
@@ -25,8 +25,14 @@ tokenizing <- function(original_files, concatenated_files){
   concatenated_files
 }
 
-folders = list.dirs('~/Documents/teste-personalidade')
+folders = list.dirs('/home/bahbbc/Documents/b5-post(confidencial)/normalised')
 
 file_names = list.files(folders, full.names=TRUE, pattern='[0-9]')
 
 tokenized_text <- tokenizing(file_names, data.frame())
+
+personalities <- read.csv('~/workspace/masters-big5/personality-normalized.csv')
+
+text_personality_matrix <- merge(tokenized_text, personalities, by.x = 'file_name', by.y ='id')
+
+write.csv(text_personality_matrix, file = 'personality-normalized-word2vec-norm.csv')
