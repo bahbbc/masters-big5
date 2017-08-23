30732afb4885c38af1a2990ad9e7a9d6c1bc8b70
Barbara Barbosa <barbarab.claudino@gmail.com>
2017-08-23 09:43:59 -0300

    last modifications

--- normalize_posts_to_file.R
+++ normalize_posts_to_file.R
@@ -3,9 +3,12 @@
 tokenizing <- function(original_files, concatenated_files){
   # if the merged dataset does exist, append to it
   for (file in original_files){
-    text <-readLines(file, encoding="latin1")
+    text <-readLines(file, encoding="utf-8")
     # Save file_name to match personality
-    file_name <- as.integer(text[1])
+    file_name <- gsub("/home/bahbbc/Documents/post-utf8/normalised-v2/post-speaker-", "", file)
+    #file_name <- gsub("/home/bahbbc/Documents/post-utf8/lemmatised/lemma-post-speaker-", "", file)
+    #file_name <- gsub("/home/bahbbc/Documents/post-utf8/normalised-v2/post-speaker-", "", file)
+    file_name <- gsub("-normalised.txt", "", file_name)
     
     # generate one whole string with the text
     formatted_text = paste(unlist(text), collapse=' ')
@@ -14,10 +17,11 @@ tokenizing <- function(original_files, concatenated_files){
     # Remove '"'
     formatted_text = gsub("\"", " ", formatted_text)
     
-    # if there is no facebook data, there will be no file name, skip to the next
     if (is.na(file_name)){
+      print('--')
       next
     }
+    
     final_text <- cbind(file_name, formatted_text)
     
     concatenated_files <- rbind(concatenated_files, final_text)
@@ -25,14 +29,14 @@ tokenizing <- function(original_files, concatenated_files){
   concatenated_files
 }
 
-folders = list.dirs('/home/bahbbc/Documents/b5-post(confidencial)/normalised')
+folders = list.dirs('/home/bahbbc/Documents/post-utf8/normalised-v2/')
 
 file_names = list.files(folders, full.names=TRUE, pattern='[0-9]')
 
 tokenized_text <- tokenizing(file_names, data.frame())
 
 personalities <- read.csv('~/workspace/masters-big5/personality-normalized.csv')
-
+  
 text_personality_matrix <- merge(tokenized_text, personalities, by.x = 'file_name', by.y ='id')
 
-write.csv(text_personality_matrix, file = 'personality-normalized-word2vec-norm.csv')
+write.csv(text_personality_matrix, file = 'personality-normalized-v2-word2vec.csv')
